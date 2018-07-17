726f0a2ff868783f0e8e047974dacb5771b29085
coord.e <me@coord-e.com>
2018-07-17 21:20:49 +0900

    Change: Use argparse to get parameters

--- generate_markers.py
+++ generate_markers.py
@@ -2,11 +2,20 @@ from cv2 import aruco
 import sys
 from PIL import Image
 import math
+import argparse
 
-num = 16
-size = 100
-padding = 10
-rows = 4
+parser = argparse.ArgumentParser(description='Generate an image containing ArUco markers')
+parser.add_argument('-n', '--num', type=int, required=True, help='The number of markers')
+parser.add_argument('-s', '--size', type=int, default=100, help='The side length of markers')
+parser.add_argument('-p', '--padding', type=int, default=10, help='The padding between markers')
+parser.add_argument('-r', '--rows', type=int, default=4, help='The number of markers in one row')
+parser.add_argument('-o', '--out', type=str, required=True, help='Output Image')
+args = parser.parse_args()
+
+num = args.num
+size = args.size
+padding = args.padding
+rows = args.rows
 cols = math.ceil(num / rows)
 
 length = size + 2 * padding
@@ -21,4 +30,4 @@ for i in range(num):
     y_pos = (i // rows) * length + padding
     canvas.paste(image, (x_pos, y_pos))
 
-canvas.save("out.png")
+canvas.save(args.out)
