bac3724d18fd628aa048d1a88c236dc23b9584e9
coord.e <me@coord-e.com>
2018-07-19 02:55:10 +0900

    Add: Implement smoothing

--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@ parser.add_argument('-i', '--input', type=str, required=True, help='Input json d
 parser.add_argument('-v', '--visualize', action="store_true", help='Show matplotlib window')
 parser.add_argument('--dpi', type=int, default=400, help='DPI of output image')
 parser.add_argument('--with-rotation', action="store_true", help='Add rotation arrows')
+parser.add_argument('-s', '--smoothing', type=int, help='Take a moving average')
 parser.add_argument('-o', '--output', type=str, required=True, help='Output image path')
 
 args = parser.parse_args()
@@ -30,8 +31,18 @@ def extract(data, key, component):
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 for key in (str(i) for i in range(8)):
+    def moving_average(a, n):
+        a = np.ma.masked_array(a, np.isnan(a))
+        ret = np.cumsum(a.filled(0), axis=0)
+        ret[n:] = ret[n:] - ret[:-n]
+        counts = np.cumsum(~a.mask, axis=0)
+        counts[n:] = counts[n:] - counts[:-n]
+        ret[~a.mask] /= counts[~a.mask]
+        ret[a.mask] = np.nan
+        return ret
+
     data = extract(indata['data'], key, 'transform')
-    x, y, z = data.T
+    x, y, z = moving_average(data, args.smoothing).T if args.smoothing else data.T
     ax.plot(x, y, z, label=key)
     if args.with_rotation:
         rotation = extract(indata['data'], key, 'rotation')
