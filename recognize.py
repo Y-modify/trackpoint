00b9c1ea38c317e597808ca23e02c258750438b1
coord.e <me@coord-e.com>
2018-07-18 21:52:11 +0900

    Add: Implement pose estimation

--- recognize.py
+++ recognize.py
@@ -8,8 +8,9 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('-i', '--input', type=str, help='Input Movie')
 group.add_argument('-c', '--camera', type=int, default=0, help='Input Camera')
 parser.add_argument('--dict', type=str, default="4X4_50", help='The ArUco marker dictionary used in the movie')
-parser.add_argument('-m', '--camera-matrix', type=str, required=True, help='THe path to npy file contains the camera matrix')
-parser.add_argument('-d', '--dist-coeff', type=str, required=True, help='THe path to npy file contains the distortion coefficients')
+parser.add_argument('-m', '--camera-matrix', type=str, required=True, help='The path to npy file contains the camera matrix')
+parser.add_argument('-d', '--dist-coeff', type=str, required=True, help='The path to npy file contains the distortion coefficients')
+parser.add_argument('-s', '--size', type=float, required=True, help='The length of the markers\' side')
 
 args = parser.parse_args()
 
@@ -30,7 +31,10 @@ while True:
 
     corners, ids, _ = aruco.detectMarkers(gray, dictionary, cameraMatrix=cameraMatrix, distCoeff=distCoeff)
 
-    result.append({int(idn): corner.tolist() for (corner, idn) in zip(corners, ids)})
+    rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, args.size, cameraMatrix, distCoeff)
+
+    result.append({int(idn): {"rotation": rvec.tolist(), "transform": tvec.tolist()} for (rvec, tvec, idn) in zip(rvecs, tvecs, ids)})
+
 
 cap.release()
 cv2.destroyAllWindows()
