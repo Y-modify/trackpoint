b1b44803b84d44bc5f3f6390558de6b3539089cc
coord.e <me@coord-e.com>
2018-07-20 12:47:59 +0900

    Add: Create an animated plot

--- plot.py
+++ plot.py
@@ -7,6 +7,7 @@ parser.add_argument('-i', '--input', type=str, required=True, help='Input json d
 parser.add_argument('-v', '--visualize', action="store_true", help='Show matplotlib window')
 parser.add_argument('--dpi', type=int, default=400, help='DPI of output image')
 parser.add_argument('--with-rotation', action="store_true", help='Add rotation arrows')
+parser.add_argument('-a', '--animation', type=str, help='Create animation')
 parser.add_argument('-s', '--smoothing', type=int, help='Take a moving average')
 parser.add_argument('-o', '--output', type=str, required=True, help='Output image path')
 
@@ -18,6 +19,7 @@ if not args.visualize:
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
 
 with open(args.input) as f:
     indata = json.load(f)
@@ -30,26 +32,44 @@ def extract(data, key, component):
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-for key in (str(i) for i in range(8)):
-    def moving_average(a, n):
-        a = np.ma.masked_array(a, np.isnan(a))
-        ret = np.cumsum(a.filled(0), axis=0)
-        ret[n:] = ret[n:] - ret[:-n]
-        counts = np.cumsum(~a.mask, axis=0)
-        counts[n:] = counts[n:] - counts[:-n]
-        ret[~a.mask] /= counts[~a.mask]
-        ret[a.mask] = np.nan
-        return ret
+def plot(i, ax, lims=None):
+    ax.cla()
+    if lims:
+        xlim, ylim, zlim = lims
+        ax.set_xlim(xlim)
+        ax.set_ylim(ylim)
+        ax.set_zlim(zlim)
 
-    data = extract(indata['data'], key, 'transform')
-    x, y, z = moving_average(data, args.smoothing).T if args.smoothing else data.T
-    ax.plot(x, y, z, label=key)
-    if args.with_rotation:
-        rotation = extract(indata['data'], key, 'rotation')
-        u, v, w = (rotation * data).T
-        ax.quiver(x, y, z, u, v, w, length=0.05, normalize=True, label=key)
+    lines = []
+    for key in [str(idx) for idx in range(8)]:
+        def moving_average(a, n):
+            a = np.ma.masked_array(a, np.isnan(a))
+            ret = np.cumsum(a.filled(0), axis=0)
+            ret[n:] = ret[n:] - ret[:-n]
+            counts = np.cumsum(~a.mask, axis=0)
+            counts[n:] = counts[n:] - counts[:-n]
+            ret[~a.mask] /= counts[~a.mask]
+            ret[a.mask] = np.nan
+            return ret
 
-ax.legend()
+        data = extract(indata['data'], key, 'transform')[:i]
+        x, y, z = moving_average(data, args.smoothing).T if args.smoothing else data.T
+        lines.append(ax.plot(x, y, z, label=key)[0])
+        if args.with_rotation:
+            rotation = extract(indata['data'], key, 'rotation')[:i]
+            u, v, w = (rotation * data).T
+            ax.quiver(x, y, z, u, v, w, length=0.05, normalize=True, label=key)
+        # ax.view_init(azim=-90., elev=-135.)
+        ax.legend()
+    return lines
+
+plot(None, ax)
+lims = (ax.get_xlim(), ax.get_ylim(), ax.get_zlim())
+
+if args.animation:
+    ani = animation.FuncAnimation(fig, plot, interval=1000/metadata['frame']['fps'], frames=int(metadata['frame']['count']), fargs=(ax,lims))
+    ani.save(args.animation, writer="ffmpeg", dpi=args.dpi)
+
 plt.savefig(args.output, dpi=args.dpi)
 if args.visualize:
     plt.show()
